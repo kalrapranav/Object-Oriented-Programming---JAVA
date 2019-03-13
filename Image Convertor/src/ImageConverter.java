b03312bc24f3c4d62786333ea0e552c16198a05a
Pranav Kalra <pranav99kalra@gmail.com>
2019-03-13 03:01:39 -0700

    Add files via upload

---
+++ Image Convertor/src/ImageConverter.java
@@ -0,0 +1,56 @@
+import java.awt.image.BufferedImage;
+
+public class ImageConverter
+{
+    private BufferedImage img;
+    private String color;
+    private String inputFilename;
+
+    public ImageConverter()
+    {
+
+    }
+
+    public void readImage(String filename)
+    {
+
+    }
+    public void toGrayscale()
+    {
+
+    }
+    public void toRed()
+    {
+
+    }
+    public void toGreen()
+    {
+
+    }
+    public void toBlue()
+    {
+
+    }
+    public void writeImage()
+    {
+        
+    }
+    public String getPixelRGB(int x, int y)
+    {
+        return "";
+
+    }
+    public String getIdentificationString()
+    {
+        return "";
+    }
+    public String toString()
+    {
+        return "";
+    }
+    public void classDemo()
+    {
+
+
+    }
+}
